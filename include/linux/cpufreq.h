9542afcea536f84ac0f562f625906c522d33fcc9
Kristof Petho <kristof.petho@gmail.com>
2014-07-03 19:31:58 +0200

    Find 7: Add Smartmax governor
    PS3:
    -unified smartmax config
    
    Change-Id: I9df66776ab1a9227736a3baea0b48877e752f5b5

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -376,6 +376,9 @@ extern struct cpufreq_governor cpufreq_gov_conservative;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVE)
 extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
+extern struct cpufreq_governor cpufreq_gov_smartmax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
 #endif
 
 
