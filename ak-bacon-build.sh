40d7c1d696b73f009fe4fc370350eb1ddd0833f2
anarkia1976 <stefano.villa1976@gmail.com>
2015-08-24 17:43:22 +0200

    Added ak-bacon-build

---
+++ ak-bacon-build.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+# Bash Color
+green='\033[01;32m'
+red='\033[01;31m'
+blink_red='\033[05;31m'
+restore='\033[0m'
+
+clear
+
+# Resources
+THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
+KERNEL="zImage"
+DTBIMAGE="dtb"
+DEFCONFIG="ak_bacon_defconfig"
+
+# Kernel Details
+BASE_AK_VER="AK"
+VER=".001.OPO.CAF"
+AK_VER="$BASE_AK_VER$VER"
+
+# Vars
+export LOCALVERSION=~`echo $AK_VER`
+export CROSS_COMPILE=${HOME}/android/AK-uber-5.2/bin/arm-eabi-
+export ARCH=arm
+export SUBARCH=arm
+export KBUILD_BUILD_USER=ak
+export KBUILD_BUILD_HOST=kernel
+
+# Paths
+KERNEL_DIR=`pwd`
+REPACK_DIR="${HOME}/android/AK-OnePone-AnyKernel2"
+PATCH_DIR="${HOME}/android/AK-OnePone-AnyKernel2/patch"
+MODULES_DIR="${HOME}/android/AK-OnePone-AnyKernel2/modules"
+ZIP_MOVE="${HOME}/android/AK-releases"
+ZIMAGE_DIR="${HOME}/android/AK-OnePone-Reborn-CAF/arch/arm/boot"
+
+# Functions
+function clean_all {
+		rm -rf $MODULES_DIR/*
+		cd $REPACK_DIR
+		rm -rf $KERNEL
+		rm -rf $DTBIMAGE
+		git reset --hard > /dev/null 2>&1
+		git clean -f -d > /dev/null 2>&1
+		cd $KERNEL_DIR
+		echo
+		make clean && make mrproper
+}
+
+function make_kernel {
+		echo
+		make $DEFCONFIG
+		make $THREAD
+		cp -vr $ZIMAGE_DIR/$KERNEL $REPACK_DIR
+}
+
+function make_modules {
+		rm `echo $MODULES_DIR"/*"`
+		find $KERNEL_DIR -name '*.ko' -exec cp -v {} $MODULES_DIR \;
+}
+
+function make_dtb {
+		$REPACK_DIR/tools/dtbToolCM -2 -o $REPACK_DIR/$DTBIMAGE -s 2048 -p scripts/dtc/ arch/arm/boot/
+}
+
+function make_zip {
+		cd $REPACK_DIR
+		zip -r9 `echo $AK_VER`.zip *
+		mv  `echo $AK_VER`.zip $ZIP_MOVE
+		cd $KERNEL_DIR
+}
+
+
+DATE_START=$(date +"%s")
+
+echo -e "${green}"
+echo "AK Kernel Creation Script:"
+echo "    _____                         "
+echo "   (, /  |              /)   ,    "
+echo "     /---| __   _   __ (/_     __ "
+echo "  ) /    |_/ (_(_(_/ (_/(___(_(_(_"
+echo " ( /                              "
+echo " _/                               "
+echo
+
+echo "---------------"
+echo "Kernel Version:"
+echo "---------------"
+
+echo -e "${red}"; echo -e "${blink_red}"; echo "$AK_VER"; echo -e "${restore}";
+
+echo -e "${green}"
+echo "-----------------"
+echo "Making AK Kernel:"
+echo "-----------------"
+echo -e "${restore}"
+
+while read -p "Do you want to clean stuffs (y/n)? " cchoice
+do
+case "$cchoice" in
+	y|Y )
+		clean_all
+		echo
+		echo "All Cleaned now."
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo
+
+while read -p "Do you want to build kernel (y/n)? " dchoice
+do
+case "$dchoice" in
+	y|Y)
+		make_kernel
+		make_dtb
+		make_modules
+		make_zip
+		break
+		;;
+	n|N )
+		break
+		;;
+	* )
+		echo
+		echo "Invalid try again!"
+		echo
+		;;
+esac
+done
+
+echo -e "${green}"
+echo "-------------------"
+echo "Build Completed in:"
+echo "-------------------"
+echo -e "${restore}"
+
+DATE_END=$(date +"%s")
+DIFF=$(($DATE_END - $DATE_START))
+echo "Time: $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds."
+echo
+
