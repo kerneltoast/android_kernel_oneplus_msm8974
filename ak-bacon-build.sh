5ea4e78d54d248628022e4126b63cf52702f4d3b
anarkia1976 <stefano.villa1976@gmail.com>
2015-08-27 09:33:54 +0200

    ak-bacon-build: compile with Uber Toolchain 5.2

--- ak-bacon-build.sh
+++ ak-bacon-build.sh
@@ -21,7 +21,7 @@ AK_VER="$BASE_AK_VER$VER"
 
 # Vars
 export LOCALVERSION=~`echo $AK_VER`
-export CROSS_COMPILE=${HOME}/android/AK-uber-4.8/bin/arm-eabi-
+export CROSS_COMPILE=${HOME}/android/AK-uber-5.2/bin/arm-eabi-
 export ARCH=arm
 export SUBARCH=arm
 export KBUILD_BUILD_USER=ak
