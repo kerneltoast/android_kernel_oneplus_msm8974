14d8d959ecb6ddedb82b09fad8252c8b5495a175
DerRomtester <dieb.stefan.96@gmail.com>
2015-10-03 14:25:34 +0200

    block: set Slice Idle to 0 BFQ/CFQ
    https://github.com/Cl3Kener/UBER-L/commit/78472fc8263c8faafd3c5d0e49b75ef73a0922cc
    https://github.com/Cl3Kener/UBER-L/commit/45e74f49e4500dd1f312ba7492af5d93dbf3591c
    
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- block/cfq-iosched.c
+++ block/cfq-iosched.c
@@ -30,7 +30,7 @@ static const int cfq_back_penalty = 2;
 static const int cfq_slice_sync = HZ / 10;
 static int cfq_slice_async = HZ / 25;
 static const int cfq_slice_async_rq = 2;
-static int cfq_slice_idle = HZ / 125;
+static int cfq_slice_idle = 0;
 static int cfq_group_idle = HZ / 125;
 static const int cfq_target_latency = HZ * 3/10; /* 300 ms */
 static const int cfq_hist_divisor = 4;
@@ -3914,7 +3914,7 @@ static int __init cfq_init(void)
 	if (!cfq_slice_async)
 		cfq_slice_async = 1;
 	if (!cfq_slice_idle)
-		cfq_slice_idle = 1;
+		cfq_slice_idle = 0;
 
 #ifdef CONFIG_CFQ_GROUP_IOSCHED
 	if (!cfq_group_idle)
