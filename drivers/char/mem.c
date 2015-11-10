22aea0153c73eae0674f32d065b992721e85c28e
Sultanxda <sultanxda@gmail.com>
2015-11-10 13:23:28 -0800

    Revert "printk: convert byte-buffer to variable-length record buffer"
    This reverts commit eae38ecef4e08c5b0da904718cc3bace912fadc2.

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -825,54 +825,33 @@ static const struct file_operations oldmem_fops = {
 static ssize_t kmsg_writev(struct kiocb *iocb, const struct iovec *iv,
 			   unsigned long count, loff_t pos)
 {
-	char *buf, *line;
+	char *line, *p;
 	int i;
-	int level = default_message_loglevel;
-	int facility = 1;       /* LOG_USER */
+	ssize_t ret = -EFAULT;
 	size_t len = iov_length(iv, count);
-	ssize_t ret = len;
 
-	if (len > 1024)
-		return -EINVAL;
-	buf = kmalloc(len+1, GFP_KERNEL);
-	if (buf == NULL)
+	line = kmalloc(len + 1, GFP_KERNEL);
+	if (line == NULL)
 		return -ENOMEM;
 
-	line = buf;
-	for (i = 0; i < count; i++) {
-		if (copy_from_user(line, iv[i].iov_base, iv[i].iov_len))
-			goto out;
-		line += iv[i].iov_len;
-	}
-
 	/*
-	* Extract and skip the syslog prefix <[0-9]*>. Coming from userspace
-	* the decimal value represents 32bit, the lower 3 bit are the log
-	* level, the rest are the log facility.
-	*
-	* If no prefix or no userspace facility is specified, we
-	* enforce LOG_USER, to be able to reliably distinguish
-	* kernel-generated messages from userspace-injected ones.
-	*/
-	line = buf;
-	if (line[0] == '<') {
-		char *endp = NULL;
-
-		i = simple_strtoul(line+1, &endp, 10);
-		if (endp && endp[0] == '>') {
-			level = i & 7;
-			if (i >> 3)
-				facility = i >> 3;
-			endp++;
-			len -= endp - line;
-			line = endp;
-		}
+	 * copy all vectors into a single string, to ensure we do
+	 * not interleave our log line with other printk calls
+	 */
+	p = line;
+	for (i = 0; i < count; i++) {
+		if (copy_from_user(p, iv[i].iov_base, iv[i].iov_len))
+			goto out;
+		p += iv[i].iov_len;
 	}
-	line[len] = '\0';
+	p[0] = '\0';
 
-	printk_emit(facility, level, NULL, 0, "%s", line);
+	ret = printk("%s", line);
+	/* printk can add a prefix */
+	if (ret > len)
+		ret = len;
 out:
-	kfree(buf);
+	kfree(line);
 	return ret;
 }
 
