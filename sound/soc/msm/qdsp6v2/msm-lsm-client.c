1ad7b1682afc14f7d571d036003214499d7f9c91
Vidyakumar Athota <vathota@codeaurora.org>
2015-12-16 23:42:39 +0000

    ASoC: msm-lsm-client: free lsm client data in msm_lsm_close
    Currently lsm client data is deallocated when q6lsm_open() fails
    which can cause memory corruption if lsm client data is accessed
    after freed. Fix this issue by deallocating the client data only
    in msm_lsm_close().
    
    Ticket RM-290
    Change-Id: If048c26a0ffd8a346a28622183cbf2ba1e7e5ff3
    Signed-off-by: Vidyakumar Athota <vathota@codeaurora.org>
    Ticket: OPO-815

--- sound/soc/msm/qdsp6v2/msm-lsm-client.c
+++ sound/soc/msm/qdsp6v2/msm-lsm-client.c
@@ -129,10 +129,10 @@ static int msm_lsm_ioctl_shared(struct snd_pcm_substream *substream,
 				if (ret < 0) {
 					pr_err("%s: lsm open failed, %d\n",
 								__func__, ret);
-					q6lsm_client_free(prtd->lsm_client);
 					kfree(prtd);
 					return ret;
 				}
+				prtd->lsm_client->opened = true;
 				pr_debug("%s: Session ID %d\n", __func__,
 					prtd->lsm_client->session);
 			} else {
@@ -619,6 +619,7 @@ static int msm_lsm_open(struct snd_pcm_substream *substream)
 		return -ENOMEM;
 	}
 	runtime->private_data = prtd;
+	prtd->lsm_client->opened = false;
 	return 0;
 }
 
@@ -668,7 +669,10 @@ static int msm_lsm_close(struct snd_pcm_substream *substream)
 				 __func__);
 	}
 
-	q6lsm_close(prtd->lsm_client);
+	if (prtd->lsm_client->opened) {
+		q6lsm_close(prtd->lsm_client);
+		prtd->lsm_client->opened = false;
+	}
 	q6lsm_client_free(prtd->lsm_client);
 
 	spin_lock_irqsave(&prtd->event_lock, flags);
@@ -676,6 +680,7 @@ static int msm_lsm_close(struct snd_pcm_substream *substream)
 	prtd->event_status = NULL;
 	spin_unlock_irqrestore(&prtd->event_lock, flags);
 	kfree(prtd);
+	runtime->private_data = NULL;
 
 	return 0;
 }
